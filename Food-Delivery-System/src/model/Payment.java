c90d1a49a827ac3d518ad756b1e2e74ccf4e79e6
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-01 09:57:24 +0530

    add payment.java

--- Food-Delivery-System/src/model/Payment.java
+++ Food-Delivery-System/src/model/Payment.java
@@ -1,5 +1,18 @@
 package model;
 
-public class Payment {
-    
+public abstract class Payment {
+    private String paymentId;
+    public Payment(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public abstract void makePayment(double amount);
 }
