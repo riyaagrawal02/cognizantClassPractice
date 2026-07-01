bd5aebb0c4a9d7b30d80076c0efceb73805d2927
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-01 09:53:46 +0530

    add foodItem.java

--- Food-Delivery-System/src/model/FoodItem.java
+++ Food-Delivery-System/src/model/FoodItem.java
@@ -1,5 +1,45 @@
 package model;
 
 public class FoodItem {
-    
+
+    private int itemId;
+    private String itemName;
+    private double price;
+    public FoodItem(int itemId, String itemName, double price) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void displayItem() {
+        System.out.println("Item ID   : " + itemId);
+        System.out.println("Item Name : " + itemName);
+        System.out.println("Price     : ₹" + price);
+        System.out.println();
+    }
 }
