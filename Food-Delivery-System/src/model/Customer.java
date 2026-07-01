1cf10d02d0c005b0b1a08e000e53898865127f40
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-01 09:47:22 +0530

    add customer.java

--- Food-Delivery-System/src/model/Customer.java
+++ Food-Delivery-System/src/model/Customer.java
@@ -1,5 +1,29 @@
 package model;
 
-public class Customer {
-    
+public class Customer extends User {
+    private String email;
+    public Customer(int userId, String name, String phoneNumber, String address, String email) {
+        super(userId, name, phoneNumber, address);
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void displayDetails() {
+
+        System.out.println("===== Customer Details =====");
+        System.out.println("Customer ID : " + getUserId());
+        System.out.println("Name        : " + getName());
+        System.out.println("Phone       : " + getPhoneNumber());
+        System.out.println("Address     : " + getAddress());
+        System.out.println("Email       : " + email);
+        System.out.println();
+    }
 }
