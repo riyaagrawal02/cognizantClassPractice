37181fb34db86e718032f1d85a2ebd822ea90d50
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-07-01 10:01:31 +0530

    add payment methods

--- Food-Delivery-System/src/model/CashPayment.java
+++ Food-Delivery-System/src/model/CashPayment.java
@@ -1,5 +1,18 @@
 package model;
 
-public class CashPayment {
-    
+public class CashPayment extends Payment {
+
+    public CashPayment(String paymentId) {
+        super(paymentId);
+    }
+
+    @Override
+    public void makePayment(double amount) {
+
+        System.out.println("\n===== CASH ON DELIVERY =====");
+        System.out.println("Payment ID : " + getPaymentId());
+        System.out.println("Amount     : ₹" + amount);
+        System.out.println("Payment Mode : Cash on Delivery");
+        System.out.println("Please pay the delivery partner upon delivery.");
+    }
 }
--- Food-Delivery-System/src/model/UpiPayment.java
+++ Food-Delivery-System/src/model/UpiPayment.java
@@ -1,5 +1,28 @@
 package model;
 
-public class UpiPayment {
-    
+public class UpiPayment extends Payment {
+    private String upiId;
+
+    public UpiPayment(String paymentId, String upiId) {
+        super(paymentId);
+        this.upiId = upiId;
+    }
+
+    public String getUpiId() {
+        return upiId;
+    }
+
+    public void setUpiId(String upiId) {
+        this.upiId = upiId;
+    }
+
+    @Override
+    public void makePayment(double amount) {
+
+        System.out.println("\n===== UPI PAYMENT =====");
+        System.out.println("Payment ID : " + getPaymentId());
+        System.out.println("UPI ID     : " + upiId);
+        System.out.println("Amount     : ₹" + amount);
+        System.out.println("Payment Successful via UPI.");
+    }
 }
